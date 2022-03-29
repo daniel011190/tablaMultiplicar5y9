e71f57af103b40cd6830ff9b135522871dd41166
daniel011190 <99054635+daniel011190@users.noreply.github.com>
2022-03-29 13:32:42 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//TABLA DE MULTIPLICAR DEL 5 y 9
+
+
+function tablas(num){
+    for (let i = 1; i <= 10 ; i++){
+        let res = num*i
+        console.log(`${num} x ${i} = ${res}`)
+    }
+}
+
+
+tablas(5)
+//tabla(9)
